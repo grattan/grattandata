3a3637c5c671965f1652361f359cac5472f99967
Matt Cowgill <mattcowgill@gmail.com>
2019-09-30 11:16:54 +1000

    create internal data

---
+++ data-raw/known datasets.R
@@ -0,0 +1,16 @@
+# This script defines known datasets (eg. `SIH`) and the providers they come 
+# from (eg. `ABS`) for use by the `read_microdata()` function. If you modify
+# the tibble that defines the datasets, you must execute this script, 
+# including the `use_data()` line, to update the internal table used by the 
+# package.
+
+datasets <- tibble::tribble(
+              ~provider,   ~release,                    ~full_title,
+                  "abs",      "sih", "Survey of Income and Housing",
+                  "abs",      "hes", "Household Expenditure Survey",
+                  "abs",      "nhs",       "National Health Survey",
+                  "ato", "taxstats",      "Individual sample files"
+              )
+
+
+usethis::use_data(datasets, internal = TRUE, overwrite = TRUE)

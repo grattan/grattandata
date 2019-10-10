ec67b5451eb6472b9cb08a5024394c24597dbfb6
Matt Cowgill <mattcowgill@gmail.com>
2019-10-11 08:47:16 +1100

    new simpler API

--- R/read_microdata.R
+++ R/read_microdata.R
@@ -1,315 +1,133 @@
 #' @title read_microdata loads data from the Grattan Institute data warehouse
+#' This function requires access to the Grattan Institute data warehouse, which
+#' is housed in the Grattan Institute Dropbox. If you do not have access to
+#' this Dropbox the function will not work.
 #'
-#' @param dataset The abbreviated name of the dataset you wish to load, such as
-#' `sih` for the ABS Survey of Income and Housing.
+#' @param filename filename
 #' 
-#' @param year The year of the dataset you wish to load, such as `2015-16` or
-#' `2012`.
+#' @param catalog_file Optional. Filename of catalogue file, including extension. 
+#' For use with SAS files that store labels in a separate catalogue file. 
+#' Must be located in the same directory as the data, or a subdirectory of 
+#' that directory.
 #' 
-#' @param level Optional. Some datasets include multiple files corresponding to 
-#' different levels - such as the household or person level. 
-#' 
-#' @param provider Optional. The provider of the dataset you wish to load, such as
-#' 'ABS' or 'ATO'. `read_microdata()` can usually figure this out for itself.
-#' 
-#' @param filetype Optional. Possible values include 'stata', 'sas', and 'csv'. 
-#' By default, `read_microdata()` will load Stata files
-#' if available, falling back to SAS files if they're not available, and CSV
-#' if neither Stata nor SAS files are present.
-#'
-#' @param path Optional. Specifying the filepath is an alternative to using the 
-#' other arguments such as 'dataset'. If specified, 'path' should be the file
-#' path to the microdata file within the `microdata` folder,
-#' including filename and extension. For example, "abs/sih/2015-16/HES15BP.dta".
-#' Use `/` to separate subdirectories, not `\`, regardless of whether you're on a
-#' Windows machine or a Mac.
-#'
-#' @param setclass default = `tbl`. Class of object you wish to be returned.
-#' Default returns a tibble. Can also specify `data.frame` or `data.table`.
+#' @param setclass A character vector specifying the format of the object you 
+#' wish to import. Default is "tbl", a tibble. Other options are "data.table" and
+#' "data.frame". See `?rio::import`.
 #'
 #' @param ... arguments passed to `rio::import()`. See `?rio::import`
 #'
 #' @examples
 #'
 #' \donttest{
-#' hes_1516 <- read_microdata(dataset = "hes", year = "2015-16", level = "p")
+#' sih_1516 <- read_microdata("SIH15bp.dta")
 #' }
 #'
 #' @importFrom rio import
 #' @importFrom jsonlite fromJSON
 #' @importFrom magrittr use_series
+#' @importFrom tools file_ext
 #'
 #' @name read_microdata
 #' @export
 
-read_microdata <- function(dataset = NULL,
-                           year = NULL, 
-                           level = NULL,
-                           provider = NULL,
-                           filetype = NULL,
-                           path = NULL,
+read_microdata <- function(filename,
+                           catalog_file = NULL,
                            setclass = "tbl",
                            ...) {
-
-  if(is.null(path) & is.null(dataset)) {
-    stop("You must specify either `path` or `dataset`.")
-  }
-
   
-  if(!is.null(dataset) & is.null(year)) {
-    stop("You must specify the year of the data you wish to load.")
-  }
+  if(class(filename) != "character") stop("`filename` must be a character string.")
   
-  if(!is.null(path) & !is.null(dataset)) {
-    warning("You have specified both 'path' and 'dataset'. Ignoring 'dataset'
-            and using 'path'.")
-    
-    dataset <- NULL
-  }
-  
+  if(length(filename) != 1) stop(paste0("`filename` has ", length(filename), " elements. ",
+                                        "It must only have one."))
 
+  # Construct path to data warehouse -----
+  
+  # Locate the .json file containing information about the user's local Dropbox
   dropbox_info_location <-
     if (Sys.getenv("OS") == "Windows_NT") {
       file.path(Sys.getenv("LOCALAPPDATA"), "Dropbox", "info.json")
     } else {
       "~/.dropbox/info.json"
     }
-
+  
   if(!file.exists(dropbox_info_location)) {
     stop("read_microdata() could not find your Dropbox location.")
   }
-
+  
+  # Construct a path on the local disk to Dropbox (business, not personal)
   dropbox_path <-
     jsonlite::fromJSON(dropbox_info_location) %>%
     magrittr::use_series("business") %>%
     magrittr::use_series("path")
-
+  
+  if(!dir.exists(dropbox_path)) {
+    stop("read_microdata() could not find the Grattan Dropbox on your local machine.")
+  }
+  
+  # Check for access to the data warehouse
   data_warehouse_path <- file.path(dropbox_path,
                                    "data",
                                    "microdata")
   
-  # Check if user has access to data warehouse
   if(file.access(data_warehouse_path, mode = 2) != 0) {
     stop(paste0("You do not appear to have access to the Grattan data
-                warehousehose,\n", data_warehouse_path))
+                warehouse,\n", data_warehouse_path))
   }
   
-  # Contruct file path from given arguments if `path` not specified
-  if(is.null(path)) {
-    
-    data_dir <- construct_path(dataset = dataset,
-                               year = year,
-                               level = level,
-                               provider = provider,
-                               filetype = filetype,
-                               data_warehouse_path = data_warehouse_path)
-    
+  # Look in all subdirs of the datawarehouse, if a single match is found, return it
+  # If 0 matches, stop; if > 1 matches, stop and tell the user what they are
+  # First, exclude files with given extensions ('unused_extensions')
+  unused_extensions <- c("zip")
+  
+  all_files <- list.files(data_warehouse_path, recursive = TRUE)
+  all_files <- all_files[!tools::file_ext(all_files) %in% unused_extensions]
+  
+  matched_files <- all_files[grepl(filename, all_files,
+                                   ignore.case = TRUE)]
+  
+  if(length(matched_files) < 1 ) {
+    stop(paste0("No matches could be found for ", filename, 
+                "\nin the Grattan data warehouse folders to which you have access"))
   }
   
-  if(is.null(path)) {
-    
-    data_dir <- file.path(data_warehouse_path, data_dir)
-  } else {
-    data_dir <- file.path(data_warehouse_path, dirname(path))
+  if(length(matched_files) > 1) {
+    stop(paste0("Multiple files were found with the filename ", filename,
+                ".\n The matches are:\n", paste0(matched_files, collapse = "\n")))
   }
   
+  path <- file.path(data_warehouse_path, matched_files)
   
-  # Check if directory exists 
-  if(!dir.exists(data_dir)) {
-    stop(paste0("Cannot find the directory: ", data_dir))
-  }
-  
-  # Check if user has access to directory
-  if(file.access(data_dir, mode = 2) != 0) {
-    stop(paste0("You do not have access to ", data_dir))
-  }
-  
-  # Get filename if not supplied
-  
-  if(is.null(path)) {
-    
-    filename <- get_filename(data_dir = data_dir,
-                             level = level,
-                             dataset = dataset,
-                             filetype = filetype)
-    
-    path <- file.path(data_dir, filename)
-  } else {
-    
-    filename <- basename(path)
-    
-    path <- file.path(data_dir, filename)
-    
-  }
-  
-  
-  # Check if file exists
+  # Check if file exists -----
   if(!file.exists(path)) {
     stop(paste0("The file ", path, " cannot be found"))
   }
   
+  # Construct catalog_file path if needed -----
+  
+  if(!is.null(catalog_file)) {
+    
+    files_in_dir <- list.files(dirname(path), recursive = TRUE)
+    
+    matched_catalog_file <- files_in_dir[grepl(catalog_file, files_in_dir, ignore.case = TRUE)]
+    
+    matched_catalog_file <- file.path(dirname(path), matched_catalog_file)
+    
+    if(length(matched_catalog_file) != 1) {
+      stop(paste0("Found ", length(matched_catalog_file), " matches for ",
+                  catalog_file, " in ", dirname(path)))
+    }
+    
+  } else { # Case where user did not specify catalog_file
+    matched_catalog_file <- NULL
+  }
 
+  # Import file ------
   message(paste0("Importing: ",
                  path))
   
   rio::import(file = path,
-              setclass = setclass,
+              catalog_file = matched_catalog_file,
+              setclass = "tbl",
               ...)
-
 }
-
-# Internal function to construct a path from various supplied arguments
-construct_path <- function(dataset,
-                           year, 
-                           level,
-                           provider,
-                           filetype,
-                           data_warehouse_path) {
-  
-  # If the user doesn't specify the data provider, we first try known datasets
-  # contained in the internal `datasets` table; if not there, we loop through 
-  # the various providers 
-  
-  if(is.null(provider)) {
-    
-    # First, check the known datasets in `datasets`
-    if(dataset %in% datasets$dataset) {
-      
-      provider <- datasets$provider[datasets$dataset == dataset]
-      
-    } else {
-      
-      all_providers <- list.dirs(data_warehouse_path, 
-                                 recursive = FALSE,
-                                 full.names = FALSE)
-      
-      # Initialise an empty vector; fill with provider name if dataset found
-      prov_with_dataset <- as.character()
-      
-      # Loop through all providers to see if the dataset can be found
-      for(prov in all_providers) {
-        
-        prov_datasets <- list.dirs(file.path(data_warehouse_path,
-                                             prov),
-                                   recursive = FALSE,
-                                   full.names = FALSE)
-        
-        if(dataset %in% prov_datasets) {
-          prov_with_dataset <- prov
-        }
-      }
-      
-      if(length(prov_with_dataset) == 0) {
-        stop(paste0("Could not find the provider for dataset ",
-                    dataset, "."))
-      }
-      
-      provider <- prov_with_dataset
-      
-    } # End of logic that applies when dataset not in `datasets` table
-    
-  } # End of logic that applies when provider isn't supplied by the user
-  
-  # If the user hasn't specified a filetype, try to find it
-  if(is.null(filetype)) {
-    
-    path_without_type <- file.path(data_warehouse_path,
-                                   provider,
-                                   dataset,
-                                   year)
-    
-    filetypes <- list.dirs(path_without_type,
-                           recursive = FALSE,
-                           full.names = FALSE)
-    
-    filetypes <- tolower(filetypes)
-    
-    if("stata" %in% filetypes) {
-      filetype <- "stata"
-    } else if("sas" %in% filetypes) {
-      filetype <- "sas"
-    } else if("csv" %in% filetype) {
-      filetype <- "csv"
-    } else {
-      stop(paste0("Could not find Stata, SAS, or CSV subfolders at ",
-                  path_without_type, "."))
-    }
-    
-  }
-  
-  path <- file.path(provider, dataset, year, filetype)
-  
-  path
-  
-} 
-
-get_filename <- function(data_dir,
-                         dataset,
-                         level,
-                         filetype) {
-  
-  files_in_dir <- list.files(data_dir, 
-                             recursive = FALSE,
-                             include.dirs = FALSE,
-                             ignore.case = TRUE)
-  
-  # If the user hasn't specified a level & there's only one file in the dir, 
-  # use that file
-  if(length(files_in_dir) == 1 & is.null(level)) {
-    
-    file <- files_in_dir[1]
-    
-  } else {
-    
-    # If there's >1 file and the user hasn't specified a level, stop 
-    if(is.null(level)) {
-      stop(paste0("There are multiple files in the directory\n",
-                  data_dir, "\nDo you need to set the 'level'?",
-                  "\nFiles in this directory are: ", 
-                  paste(files_in_dir, collapse = ", ")))
-    } else {
-      
-      # User has specified level - find the file that ends in the specified level
-      file <- files_in_dir[endsWith(tools::file_path_sans_ext(tolower(files_in_dir)),
-                                    tolower(level))]
-      
-      
-      # If more than one file matches the 'level' pattern, find one that includes
-      # the 'dataset' string at the start 
-      # (so if, eg. hes15bp.dta and sih15bp.dta are present,
-      # and the user has requested dataset 'sih', return sih15bp.dta)
-      
-      if(length(file) > 1) {
-        
-        files_in_dir_with_dataset <- files_in_dir[startsWith(files_in_dir, dataset)]
-        
-        file <- files_in_dir_with_dataset[endsWith(tools::file_path_sans_ext(tolower(files_in_dir_with_dataset)),
-                                      tolower(level))]
-        
-        # If we still match more than one file after filtering to files that match
-        # both the dataset and level, stop 
-        if(length(file) > 1) {
-          stop(paste0("More than one file in folder\n", data_dir,
-                      "\nends with level ", level, " and starts with ",
-                      dataset))
-        }
-      }
-     
-      # Check to see if a file exists with specified level - return an error if not
-      if(length(file) == 0) {
-        stop(paste0("Could not find a file with level '", level, "' in ",
-                    data_dir, "\nFiles in this directory are: ", 
-                    paste(files_in_dir, collapse = ", ")))
-        
-      } 
-      
-    } 
-    
-    
-  } 
-  
-  file
-  
-}
-
 
--- tests/testthat/test-read_microdata.R
+++ tests/testthat/test-read_microdata.R
@@ -2,10 +2,52 @@ test_that("read_microdata loads SIH 2015-16", {
 
   skip_on_travis()
 
-  sih_15_16 <- read_microdata("ABS/SIH/2015-16/Stata/SIH15BH.dta")
+  sih_15_16 <- read_microdata("SIH15BH.dta")
 
   expect_is(sih_15_16, "tbl_df")
 
   expect_equal(nrow(sih_15_16), 17768)
+  
+  sih_15_16_mixed_case <- read_microdata("sIh15bH.dta")
 
+  expect_true(all.equal(sih_15_16, sih_15_16_mixed_case))
+  
+  sih_15_16_withpath <- read_microdata("abs/SIH/2015-16/Stata/SIH15BH.dta")
+  
+  expect_true(all.equal(sih_15_16, sih_15_16_withpath))
+  
+})
+
+test_that("read_microdata fails with multiple matches", {
+  
+  skip_on_travis()
+  
+  expect_error(read_microdata("SIH15BH"))
+  
+})
+  
+test_that("read_microdata fails with no matches", {
+  
+  skip_on_travis()
+  
+  fake_filename <- paste0(paste0(sample(letters, 10), collapse = ""), ".dta")
+  
+  expect_error(read_microdata(fake_filename))
+})
+
+test_that("read_microdata fails with non-character filename", {
+  
+  skip_on_travis()
+  
+  expect_error(read_microdata(1))
+  
+})
+  
+
+test_that("read_microdata fails with vector input to filename", {
+  
+  skip_on_travis()
+  
+  expect_error(read_microdata(c("SIH15bh.dta", "SIH15bp.dta")))
+  
 })
